f5dd41e5ab603c8cb63625b1443e0352dfd69b8d
Michael Bowman <bowmanmc@gmail.com>
2013-11-17 19:35:45 -0500

    Fixing broken unit tests and switching to PhantomJS for testing

--- test/spec/controllers/delete.js
+++ test/spec/controllers/delete.js
@@ -1,36 +1,44 @@
 'use strict';
 
 describe('Controller: HabitDeleteCtrl', function () {
-    var DeleteCtrl, scope, location, localStorage;
-    var habitKey = 'habit.1234';
+
+    var DeleteCtrl, scope, location, habitKey, habitSvc, chainSvc;
+
     beforeEach(module('habitual'));
+    beforeEach(module('habitual.services.habit'));
+    beforeEach(module('habitual.services.chain'));
+    
+    var routeParamsStub = jasmine.createSpy('routeParamsStub');
 
     /** When there is nothing in local storage **/
-    beforeEach(inject(function($controller, $rootScope, $location, localStorageService) {
+    beforeEach(inject(function($controller, $rootScope, $location, $routeParams, habitService, chainService) {
         scope = $rootScope.$new();
         location = $location;
-        localStorage = localStorageService;
-        localStorage.clearAll();
-        localStorage.add(habitKey, {foo: 'bar'});
+        habitSvc = habitService;
+        chainSvc = chainService;
+        habitSvc.clearHabitual();
+        var habit = habitSvc.createNewHabit('Write tests first.');
+        routeParamsStub.habitId = habit.id;
         DeleteCtrl = $controller('HabitDeleteCtrl', {
             $scope: scope,
             $location: location,
-            localStorageService: localStorage
+            $routeParams: routeParamsStub,
+            habitService: habitSvc,
+            chainService: chainSvc
         });
     }));
 
-    it('Should have a habit initially', function() {
-        var r = localStorage.get(habitKey);
-        expect(r).toBeDefined();
+    it('should have one item in the habits array initially', function() {
+        expect(habitSvc.getHabits().length).toBe(1);
     });
 
     // execute the deleteHabit method
     it('Should remove the habit', function() {
         scope.deleteHabit(habitKey);
-        var r = localStorage.get(habitKey);
+
         // todo -- why doesn't this work? works in the browser...
         // must not have jasmine setup correctly...
-        //expect(r).not.toBeDefined();
+        //expect(habitSvc.getHabits().length).toBe(0);
         expect(location.path()).toBe('/');
     });
 
