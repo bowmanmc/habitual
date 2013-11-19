eca411c3fda6fc687ae4b14475f83fe8e89b81e8
Michael Bowman <bowmanmc@gmail.com>
2013-11-18 21:13:51 -0500

    Design work on the navbar.

--- app/scripts/directives/navbar.js
+++ app/scripts/directives/navbar.js
@@ -5,7 +5,7 @@ var navbarModule = angular.module('habitual.directives.navbar', []);
 navbarModule.directive('hbtNavbar', function() {
     return {
         restrict: 'EA',
-        templateUrl: 'views/navbar.html',
+        templateUrl: 'views/nav.html',
         controller: 'NavCtrl',
         link: function (scope, element, attrs) {
             scope.buttonType = attrs.buttonType;
